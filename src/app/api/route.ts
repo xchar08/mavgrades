3ec24f0d2cff2426fc455f1422638bdc70fa58aa
Atiqur Rahman <atiqur.main@gmail.com>
2024-10-20 00:21:34 -0500

    update contact email

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,11 +1,14 @@
 type Params = {
-  team: string
-}
- 
+  team: string;
+};
+
 export async function GET(request: Request, context: { params: Params }) {
   try {
-    //TODO: Add more specific route information 
-    return Response.json({ success: true, data: "Hi! It looks like you've stumbled across our API! If you're trying to use or navigate the api, contact `contact@ryanlahlou.com` for details!" });
+    //TODO: Add more specific route information
+    return Response.json({
+      success: true,
+      data: "Hi! It looks like you've stumbled across our API! If you're trying to use or navigate the api, contact `acm.uta@gmail.com` for details!",
+    });
   } catch (error) {
     return Response.json({ success: false, error: error });
   }
